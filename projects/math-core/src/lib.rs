6d4ae2a85b75a49ccd284c4fd56362fddef7ac61
Aster <galaster@foxmail.com>
2022-03-07 21:39:12 +0800

    Define Constant & Function

--- projects/math-core/src/lib.rs
+++ projects/math-core/src/lib.rs
@@ -1,6 +1,56 @@
-mod taylor;
-mod errors;
+pub trait Constant {
+    fn eval(&self) -> ASTNode;
+}
 
-pub use errors::{Error, Result};
+pub trait Function {
+    fn eval(&self, args: &[ASTNode]) -> ASTNode;
+}
 
-pub use self::taylor::Taylor;
+
+pub enum ASTKind {
+    Constant {
+        inner: Box<dyn Constant>,
+    },
+    Function {
+        head: Box<dyn Function>,
+        rest: Vec<ASTNode>,
+    },
+}
+
+pub struct Span {
+    pub start: u32,
+    pub end: u32,
+}
+
+pub struct ASTNode {
+    kind: ASTKind,
+    span: Span,
+}
+
+pub struct FactorInteger {}
+
+pub struct Factorial {}
+
+impl ASTKind {
+    pub fn eval(&self) -> ASTKind {
+        match self {
+            Self::Constant { .. } => {}
+            Self::Function { .. } => {}
+        }
+    }
+}
+
+impl Function for Factorial {
+    fn eval(&self, args: &[ASTNode]) -> ASTNode {
+        todo!()
+    }
+}
+
+
+impl Function for FactorInteger {
+    fn eval(&self, args: &[ASTNode]) -> ASTNode {
+        todo!()
+    }
+}
+
+
